323dab5db37a5ba8cc3bb7ba91e122f9ce64e682
Paulo Vicente Torres Gaviño <paulo@lacafetalab.pe>
2021-01-20 16:17:21 -0500

    add view login.

--- app.js
+++ app.js
@@ -4,7 +4,10 @@
 const express = require('express')
 // Parser para el cuerpo de las peticiones.
 const bodyParser = require('body-parser')
+// Routes.
 const api = require('./routes')
+// Templates.
+const hbs = require('express-handlebars')
 
 // Crear app.
 const app = express()
@@ -13,5 +16,15 @@ const app = express()
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use('/api', api)
+app.engine('.hbs', hbs({
+    defaultLayout: 'default',
+    extname: '.hbs'
+}))
+app.set('view engine', '.hbs')
+
+// Views.
+app.get('/login', (request, response) => {
+    response.render('login')
+})
 
 module.exports = app
